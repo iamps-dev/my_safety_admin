6824aa216d3851eee7b1c8f2bb8955e781dcae71
Paresh Songire <iamps.software@gmail.com>
2026-01-07 00:57:44 +0530

    4-1-2026

--- lib/Data/Appurl/app_url.dart
+++ lib/Data/Appurl/app_url.dart
@@ -1,7 +1,7 @@
 class AppUrl {
   static const String baseUrl = "http://192.168.43.135:8080/api";
 
-  // 🔹 Admin base path
+  // 🔹 Admin base path aal done
   static const String adminAuth = "$baseUrl/auth/admin";
 
 
